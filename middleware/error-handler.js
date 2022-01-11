58eba7aa121eb7feb5f4910189b144d75e8aeed9
IMPrimph <vishnugajulapalli123@gmail.com>
2022-01-11 15:57:55 +0530

    handling missing field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,6 +6,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
         msg: "Something went wrong, try again later"
     }
+    if (err.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // this err.message comes from the error object that mongoose throws
+        defaultError.msg = Object.values(err.errors).map((item) => item.message).join(",")
+    }
+    // res.status(defaultError.statusCode).json({
+    //     msg: defaultError.msg
+    // })
     res.status(defaultError.statusCode).json({
         msg: err
     })
