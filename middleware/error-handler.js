78b486ca131e01093d1421ccd948b80d9ca52e3b
IMPrimph <vishnugajulapalli123@gmail.com>
2022-01-11 16:34:49 +0530

    handling empty field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,7 +4,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err)
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: "Something went wrong, try again later"
+        // we are checking whether err.message is already present
+        // that is it name or email or password is empty
+        // if they are already filled, then check for other errors
+        msg: err.message || "Something went wrong, try again later"
     }
 
     if (err.name === 'ValidationError') {
