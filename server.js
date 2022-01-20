c50fdb2bbcd5b2eed7200df161e3e57927277141
IMPrimph <vishnugajulapalli123@gmail.com>
2022-01-20 20:37:39 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ import connectDB from './db/connect.js'
 // to skip using try and catch blocks
 import 'express-async-errors'
 import morgan from 'morgan'
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 import authenticateUser from './middleware/auth.js'
 
@@ -21,16 +28,24 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(cors())
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
 // make data from body available
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+
 
 // middleware
-app.get("/", (req, res) => {
-    res.send('Welcome')
-})
-
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
